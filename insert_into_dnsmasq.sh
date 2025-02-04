457eb228abf4778f6c6167cf8efc7512daa0b048
nick <babin72nick@gmail.com>
2025-02-04 13:43:37 +0200

    Update insert_into_dnsmasq.sh

--- insert_into_dnsmasq.sh
+++ insert_into_dnsmasq.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #insert_into_dnsmasq.sh
 
-echo “ \
-interface=eth1 \
-dhcp-range=192.168.50.100,192.168.50.200,infinite \
-dhcp-option=option:router,192.168.50.1 \
-dhcp-option=option:dns-server,8.8.8.8 \
-log-queries \
-log-dhcp \
-“ >> /etc/dnsmasq.conf
+FILE_DNSMASQ="/etc/dnsmasq.conf"
+
+echo " " >> $FILE_DNSMASQ
+echo "interface=eth1" >> $FILE_DNSMASQ
+echo "dhcp-range=192.168.50.100,192.168.50.200,infinite" >> $FILE_DNSMASQ
+echo "dhcp-option=option:router,192.168.50.1" >> $FILE_DNSMASQ
+echo "dhcp-option=option:dns-server,8.8.8.8" >> $FILE_DNSMASQ
+echo "log-queries" >> $FILE_DNSMASQ
+echo "log-dhcp" >> $FILE_DNSMASQ
