6c04f2ad04a31648ca723b5bb4414e11df4d7a3c
nick <babin72nick@gmail.com>
2025-02-06 19:26:06 +0200

    Update metrics_eth0.sh

--- metrics_eth0.sh
+++ metrics_eth0.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # metrics_eth0.sh
 
-ifconfig | grep eth0
-ifconfig eth0 | grep TX
-ifconfig eth0 | grep RX
+FILE_LOG="/var/log/metrics_eth0.log"
+
+echo "-----START PROCESSING" >> $FILE_LOG
+echo "DATE=`date`" >> $FILE_LOG
+echo "---" >> $FILE_LOG
+
+ifconfig | grep eth0 >> $FILE_LOG
+ifconfig eth0 | grep TX >> $FILE_LOG
+ifconfig eth0 | grep RX >> $FILE_LOG
+
+echo "+++++STOP PROCESSING" >> $FILE_LOG
