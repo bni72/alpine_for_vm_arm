19285fc7d8d5ec717e4cb5a935b172e9ab01c54a
nick <babin72nick@gmail.com>
2025-02-06 19:27:31 +0200

    Update metrics_eth2.sh

--- metrics_eth2.sh
+++ metrics_eth2.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # metrics_eth2.sh
 
-ifconfig | grep eth2
-ifconfig eth2 | grep TX
-ifconfig eth2 | grep RX
+FILE_LOG="/var/log/metrics_eth2.log"
+
+echo "-----START PROCESSING" >> $FILE_LOG
+echo "DATE=`date`" >> $FILE_LOG
+echo "---" >> $FILE_LOG
+
+ifconfig | grep eth2 >> $FILE_LOG
+ifconfig eth2 | grep TX >> $FILE_LOG
+ifconfig eth2 | grep RX >> $FILE_LOG
+
+echo "+++++STOP PROCESSING" >> $FILE_LOG
