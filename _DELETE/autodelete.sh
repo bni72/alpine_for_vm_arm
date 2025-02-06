428a91b15ccafb0bd572ac1ffcaa052a6b0b72a4
nick <babin72nick@gmail.com>
2025-02-06 15:24:31 +0200

    Update autodelete.sh

--- _DELETE/autodelete.sh
+++ _DELETE/autodelete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # autodelete.sh
 
+mkdir /root/ROOT_SCRIPT ; cp /root/alpine_for_vm_arm/*.sh /root/ROOT_SCRIPT ; chmod +x /root/ROOT_SCRIPT/*.sh ; rm -R /root/alpine_for_vm_arm ; cd /root ; ls /root -silah
