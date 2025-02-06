c0d09d6a4fe531cf112081458dddcbd5d6a5aa30
nick <babin72nick@gmail.com>
2025-02-06 14:50:06 +0200

    Update install_pro.sh

--- install_pro.sh
+++ install_pro.sh
@@ -8,7 +8,3 @@ apk add curl
 apk add wget
 apk add iptables
 apk add nginx
-
-# Make folder for scripts ROOT_SCRIPT
-cd /root
-mkdir ROOT_SCRIPT ; cp /root/alpine_for_vm_arm/*.sh /root/ROOT_SCRIPT ; chmod +x /root/ROOT_SCRIPT/*.sh
