58eec42e3a41996bf4bc374f45206ec9f9488092
nick <babin72nick@gmail.com>
2025-02-07 17:23:07 +0200

    Update bd_check_50_100.sh

--- bd_check_50_100.sh
+++ bd_check_50_100.sh
@@ -8,16 +8,16 @@ EXTERNAL_INTERFACE="enp0s3"
 EXTERNAL_PORT="8822"
 
 if nc -z -w3 $TARGET_IP $TARGET_PORT; then
-    echo "Target $TARGET_IP:$TARGET_PORT is reachable."
+    echo "Target $TARGET_IP:$TARGET_PORT is YES"
     if ! iptables -t nat -C PREROUTING -i $EXTERNAL_INTERFACE -p tcp --dport $EXTERNAL_PORT -j DNAT --to-destination $TARGET_IP:$TARGET_PORT 2>/dev/null; then
-        echo "Adding iptables rule for $EXTERNAL_PORT -> $TARGET_IP:$TARGET_PORT"
+        echo "Adding iptables rules for $EXTERNAL_PORT -> $TARGET_IP:$TARGET_PORT"
         iptables -t nat -A PREROUTING -i $EXTERNAL_INTERFACE -p tcp --dport $EXTERNAL_PORT -j DNAT --to-destination $TARGET_IP:$TARGET_PORT
         iptables -A FORWARD -p tcp -d $TARGET_IP --dport $TARGET_PORT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
     fi
 else
-    echo "Target $TARGET_IP:$TARGET_PORT is NOT reachable."
+    echo "Target $TARGET_IP:$TARGET_PORT is NOT"
     if iptables -t nat -C PREROUTING -i $EXTERNAL_INTERFACE -p tcp --dport $EXTERNAL_PORT -j DNAT --to-destination $TARGET_IP:$TARGET_PORT 2>/dev/null; then
-        echo "Removing iptables rule for $EXTERNAL_PORT -> $TARGET_IP:$TARGET_PORT"
+        echo "Removing iptables rules for $EXTERNAL_PORT -> $TARGET_IP:$TARGET_PORT"
         iptables -t nat -D PREROUTING -i $EXTERNAL_INTERFACE -p tcp --dport $EXTERNAL_PORT -j DNAT --to-destination $TARGET_IP:$TARGET_PORT
         iptables -D FORWARD -p tcp -d $TARGET_IP --dport $TARGET_PORT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
     fi
